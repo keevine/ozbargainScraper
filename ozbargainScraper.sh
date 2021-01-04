8376353be4e2e9d89c8d7dd00ec103218f4e1132
Kevin Chau <kevin.chau5@outlook.com>
2021-01-04 16:14:02 +1100

    added more logic to include more search terms

--- ozbargainScraper.sh
+++ ozbargainScraper.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
 
+# Usage
+if [ "$#" -eq 0 ]
+then
+    echo "Usage: $0 [searchTerms...]"
+    exit 1
+fi
+
 webDataFile="ozbargainData.html"
 
-searchTerm="amazon"
 > temp.html
 
-# Extract html from ozbargain website
-curl https://www.ozbargain.com.au/deals > "$webDataFile"
+# Extract html from ozbargain websiteq
+curl -s https://www.ozbargain.com.au/deals > "$webDataFile"
+
+# Construct the search term with matching given keywords
+firstSearchTerm=$1
+shift
+
+regex="class=\"title\" id=.* data-title=\".*$firstSearchTerm.*\""
+echo "Searching ozBargain webpage for the given input search terms:"
+echo "$firstSearchTerm"
+for searchTerm in "$@"
+do  
+    echo "$searchTerm"
+    regex=$regex"|class=\"title\" id=.* data-title=\".*$searchTerm.*\""
+    # echo $regex
+done
 
 
+# Loop through ozbargain web data to find matching terms
+numMatches=0
 while read line 
 do 
     # Ignore the sidebar info to speed up process
@@ -20,14 +42,16 @@ do
     fi
 
     # Search for matches 
-    match=`echo "$line" | egrep -i "class=\"title\" id=.* data-title=\".*$searchTerm.*\""`
+    match=`echo "$line" | egrep -i "$regex"`
     if [ "$match" != "" ]
     then 
+        numMatches=$((numMatches+1))
         echo "$match" >> temp.html
         node=`echo "$match" | grep -P "href=\"\/node\/([0-9]+)\"" -o | sed -E "s/href=\"\/node\/|\"$//g"`
     fi
 done < "$webDataFile"
 
+echo "Exiting program with $numMatches matching posts found."
 
 #echo $node
 #echo $data > temp.html
