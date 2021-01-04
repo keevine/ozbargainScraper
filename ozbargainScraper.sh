a98c61a71f52b105baa3232c0f1acd167c70cc90
Kevin Chau <kevin.chau5@outlook.com>
2021-01-04 14:00:25 +1100

    picking up where i left off

--- ozbargainScraper.sh
+++ ozbargainScraper.sh
@@ -3,22 +3,28 @@
 webDataFile="ozbargainData.html"
 
 searchTerm="amazon"
+> temp.html
 
-
+# Extract html from ozbargain website
 curl https://www.ozbargain.com.au/deals > "$webDataFile"
+
 
 while read line 
 do 
+    # Ignore the sidebar info to speed up process
     sidebar=`echo $line | egrep "<ul class=\"ozblist\">"`
     if [ "$sidebar" != "" ]
     then
         echo "stopping..."
         break
     fi
+
+    # Search for matches 
     match=`echo "$line" | egrep -i "class=\"title\" id=.* data-title=\".*$searchTerm.*\""`
     if [ "$match" != "" ]
     then 
         echo "$match" >> temp.html
+        node=`echo "$match" | grep -P "href=\"\/node\/([0-9]+)\"" -o | sed -E "s/href=\"\/node\/|\"$//g"`
     fi
 done < "$webDataFile"
 
